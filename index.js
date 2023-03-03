ffb060b6fbeaaed255963dc0486ac48946a797fa
YASEEN-FEROZ\mdyaseenferoz <mdyaseenferoz@outlook.com>
2023-03-03 22:03:12 +0530

    temp locaction

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cloudinary = require('cloudinary').v2;
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: '/temp' });
 
 const app = express();
 
